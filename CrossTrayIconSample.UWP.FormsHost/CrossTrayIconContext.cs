0594120ac927ae089fbf409de586a0b07885c96d
Konrad Mueller <k.l.mueller@outlook.com>
2019-11-02 19:46:28 +0100

    Add uwp sample

---
+++ CrossTrayIconSample.UWP.FormsHost/CrossTrayIconContext.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
+
+namespace CrossTrayIconSample.UWP.FormsHost
+{
+    public class CrossTrayIconContext : ApplicationContext
+    {
+        private AppServiceConnection connection = null;
+        private NotifyIcon notifyIcon = null; 
+
+        public CrossTrayIconContext()
+        {
+            MenuItem openMenuItem = new MenuItem("Open UWP", new EventHandler(OpenApp));
+            MenuItem sendMenuItem = new MenuItem("Send message to UWP", new EventHandler(SendToUWP)); 
+            MenuItem exitMenuItem = new MenuItem("Exit", new EventHandler(Exit));
+            openMenuItem.DefaultItem = true;
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.DoubleClick += new EventHandler(OpenApp);
+            notifyIcon.Icon = CrossTrayIconSample.UWP.FormsHost.Resources.TrayIcon;
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[] { openMenuItem, sendMenuItem, exitMenuItem });
+            notifyIcon.Visible = true;
+        }
+
+        private async void OpenApp(object sender, EventArgs e)
+        {
+            IEnumerable<AppListEntry> appListEntries = await Package.Current.GetAppListEntriesAsync();
+            await appListEntries.First().LaunchAsync();
+        }
+
+        private async void SendToUWP(object sender, EventArgs e)
+        {
+            ValueSet message = new ValueSet();
+            message.Add("content", "Message from Systray Extension");
+            await SendToUWP(message);
+        } 
+
+        private async void Exit(object sender, EventArgs e)
+        {
+            ValueSet message = new ValueSet();
+            message.Add("exit", "");
+            await SendToUWP(message);
+            Application.Exit();
+        }
+
+        private async Task SendToUWP(ValueSet message)
+        {
+            if (connection == null)
+            {
+                connection = new AppServiceConnection();
+                connection.PackageFamilyName = Package.Current.Id.FamilyName;
+                connection.AppServiceName = "SystrayExtensionService";
+                connection.ServiceClosed += Connection_ServiceClosed;
+                AppServiceConnectionStatus connectionStatus = await connection.OpenAsync();
+                if (connectionStatus != AppServiceConnectionStatus.Success)
+                {
+                    MessageBox.Show("Status: " + connectionStatus.ToString());
+                    return;
+                }
+            }
+
+            await connection.SendMessageAsync(message);
+        }
+
+        private void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            connection.ServiceClosed -= Connection_ServiceClosed;
+            connection = null;
+        }
+    }
+}
